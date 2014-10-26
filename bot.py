498e58c9140ecd177a9bc725e6eafebb68382e36
Blade Nelson <powderblock@gmail.com>
2014-10-25 23:00:54 -0400

    Only show Imgur URLs
    Add clause to in if statement to look for imgur links that have .png or
    .jpg formats.

--- bot.py
+++ bot.py
@@ -2,5 +2,6 @@ import praw
 
 r = praw.Reddit('/u/powderblock Glasses Bot')
 
-for post in r.get_subreddit('all').get_new(limit=5):
-	print(str(post.url))
+for post in r.get_subreddit('all').get_new(limit=1):
+        if "imgur.com" in post.url and (".jpg" in post.url or ".png" in post.url):
+                        print str(post.url)
