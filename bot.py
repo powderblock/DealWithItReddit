55ab54a63844c65b10e1ea96d822198d9f25ba14
Blade Nelson <powderblock@gmail.com>
2014-10-26 03:15:33 -0400

    Save images to file

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import praw
-
+import urllib
 r = praw.Reddit('/u/powderblock Glasses Bot')
 
-for post in r.get_subreddit('all').get_new(limit=1):
+for post in r.get_subreddit('all').get_new(limit=5):
         if "imgur.com" in post.url and (".jpg" in post.url or ".png" in post.url):
                         print str(post.url)
+                        urllib.urlretrieve(str(post.url), "image.jpg")
