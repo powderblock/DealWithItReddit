d8c6f429c875a2cfdc5d520d91ea9d3a37b33ac9
Blade Nelson <powderblock@gmail.com>
2014-10-26 13:49:37 -0400

    Load image from URL into buffer
    Feed image into array, convert it, display it.

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 import praw
 import urllib
 import cv2, numpy as np
+from PIL import Image
 
 DOWNSCALE = 2
 
 r = praw.Reddit('/u/powderblock Glasses Bot')
 
 foundImage = False
- 
+
 for post in r.get_subreddit('all').get_new(limit=15):
         if "imgur.com" in post.url and (".jpg" in post.url or ".png" in post.url):
                         print str(post.url)
@@ -15,18 +16,21 @@ for post in r.get_subreddit('all').get_new(limit=15):
                         break
 
 if foundImage:
-        urllib.urlretrieve(str(post.url), "image.jpg")
+        response = urllib.urlopen(str(post.url))
         # load the image we want to detect features on
-        frame = cv2.imread('image.jpg')
+        # Convert rawImage to Mat
+        filearray = np.asarray(bytearray(response.read()), dtype=np.uint8)
+        frame = cv2.imdecode(filearray, cv2.CV_LOAD_IMAGE_UNCHANGED)
         minisize = (frame.shape[1]/DOWNSCALE,frame.shape[0]/DOWNSCALE)
         miniframe = cv2.resize(frame, minisize)
-        cv2.imshow("Loading Images From a Buffer From a URL", miniframe)
+        cv2.imshow("Loading Image Buffer From a URL", miniframe)
 
         while True:
             # key handling (to close window)
             key = cv2.waitKey(20)
             if key in [27, ord('Q'), ord('q')]: # exit on ESC
-                cv2.destroyWindow("Facial Features Test")
+                cv2.destroyWindow("Loading Image Buffer From a URL")
                 break
+
 if not foundImage:
         print("No Image found.")
