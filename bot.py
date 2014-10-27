80db2d87f227edca8ae6d6a4ed9c6c94b0c682b5
Blade Nelson <powderblock@gmail.com>
2014-10-26 22:26:52 -0400

    Lower max new count

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ foundImage = False
 
 r = praw.Reddit('/u/powderblock Glasses Bot')
 
-for post in r.get_subreddit('all').get_new(limit=30):
+for post in r.get_subreddit('all').get_new(limit=1):
         if "imgur.com" in post.url and (".jpg" in post.url or ".png" in post.url):
                         response = urllib.urlopen(str(post.url))
                         # load the image we want to detect features on
