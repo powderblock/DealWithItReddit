564c7e38cbbd97da5fd2d73d8e64d434185c59b4
Blade Nelson <powderblock@gmail.com>
2014-10-26 22:26:23 -0400

    Add glasses if eyes are found
    If eyes are found in the image, add glasses to them!

--- bot.py
+++ bot.py
@@ -4,27 +4,44 @@ import cv2, numpy as np
 from PIL import Image
 
 eyeData = "xml/eyes.xml"
+eyeClass = cv2.CascadeClassifier(eyeData)
 glasses = cv2.imread('assets/glasses.png', cv2.IMREAD_UNCHANGED)
-DOWNSCALE = 2
+ratio = glasses.shape[1] / glasses.shape[0]
+DOWNSCALE = 4
 foundImage = False
 
 r = praw.Reddit('/u/powderblock Glasses Bot')
 
-for post in r.get_subreddit('all').get_new(limit=15):
+for post in r.get_subreddit('all').get_new(limit=30):
         if "imgur.com" in post.url and (".jpg" in post.url or ".png" in post.url):
-                        print str(post.url)
-                        foundImage = True
-                        break
+                        response = urllib.urlopen(str(post.url))
+                        # load the image we want to detect features on
+                        # Convert rawImage to Mat
+                        filearray = np.asarray(bytearray(response.read()), dtype=np.uint8)
+                        frame = cv2.imdecode(filearray, cv2.CV_LOAD_IMAGE_UNCHANGED)
+                        minisize = (frame.shape[1]/DOWNSCALE,frame.shape[0]/DOWNSCALE)
+                        miniframe = cv2.resize(frame, minisize)
+                        eyes = eyeClass.detectMultiScale(miniframe)
+                        if len(eyes) > 0:
+                                print str(post.url)
+                                foundImage = True
+                                break
 
+# Only process if the image was found
 if foundImage:
-        response = urllib.urlopen(str(post.url))
-        # load the image we want to detect features on
-        # Convert rawImage to Mat
-        filearray = np.asarray(bytearray(response.read()), dtype=np.uint8)
-        frame = cv2.imdecode(filearray, cv2.CV_LOAD_IMAGE_UNCHANGED)
-        minisize = (frame.shape[1]/DOWNSCALE,frame.shape[0]/DOWNSCALE)
-        miniframe = cv2.resize(frame, minisize)
-        cv2.imshow("Loading Image Buffer From a URL", miniframe)
+        for eye in eyes:
+            x, y, w, h = [v * DOWNSCALE for v in eye]
+            h = w / ratio
+            y += h / 2
+            # resize glasses to a new var called small glasses
+            smallglasses = cv2.resize(glasses, (w, h))
+            # the area you want to change
+            bg = frame[y:y+h, x:x+w]
+            bg *= np.atleast_3d(255 - smallglasses[:, :, 3])/255.0
+            bg += smallglasses[:, :, 0:3] * np.atleast_3d(smallglasses[:, :, 3])
+            # put the changed image back into the scene
+            frame[y:y+h, x:x+w] = bg
+        cv2.imwrite(str(post.url), frame)
 
         while True:
             # key handling (to close window)
