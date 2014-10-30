877d40df2585e444f566c2ea635f622097d3b343
Blade Nelson <powderblock@gmail.com>
2014-10-29 20:25:37 -0400

    Add delay, check if done, remove closing window
    Login using getpass.
    Run the script on a delay of 30 seconds, do this with the time module.
    If the image being processed has eyes in the image, add glasses.png over
    the eyes.
    Append current image to already_done to prevent doing the same image
    more than once.
    Check width and height of the image once processing.

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import praw
 import urllib
 import cv2, numpy as np
 from PIL import Image
+import time
+import getpass
 
 eyeData = "xml/eyes.xml"
 eyeClass = cv2.CascadeClassifier(eyeData)
@@ -10,45 +12,53 @@ ratio = glasses.shape[1] / glasses.shape[0]
 DOWNSCALE = 4
 foundImage = False
 
-r = praw.Reddit('/u/powderblock Glasses Bot')
+already_done = []
 
-for post in r.get_subreddit('all').get_new(limit=1):
-        if "imgur.com" in post.url and (".jpg" in post.url or ".png" in post.url):
-                        response = urllib.urlopen(str(post.url))
-                        # load the image we want to detect features on
-                        # Convert rawImage to Mat
-                        filearray = np.asarray(bytearray(response.read()), dtype=np.uint8)
-                        frame = cv2.imdecode(filearray, cv2.CV_LOAD_IMAGE_UNCHANGED)
-                        minisize = (frame.shape[1]/DOWNSCALE,frame.shape[0]/DOWNSCALE)
-                        miniframe = cv2.resize(frame, minisize)
-                        eyes = eyeClass.detectMultiScale(miniframe)
-                        if len(eyes) > 0:
-                                print str(post.url)
-                                foundImage = True
-                                break
+password = getpass.getpass("Reddit password: ")
 
-# Only process if the image was found
-if foundImage:
-        for eye in eyes:
-            x, y, w, h = [v * DOWNSCALE for v in eye]
-            h = w / ratio
-            y += h / 2
-            # resize glasses to a new var called small glasses
-            smallglasses = cv2.resize(glasses, (w, h))
-            # the area you want to change
-            bg = frame[y:y+h, x:x+w]
-            bg *= np.atleast_3d(255 - smallglasses[:, :, 3])/255.0
-            bg += smallglasses[:, :, 0:3] * np.atleast_3d(smallglasses[:, :, 3])
-            # put the changed image back into the scene
-            frame[y:y+h, x:x+w] = bg
-        cv2.imwrite(str(post.url), frame)
+while True:
+        foundImage = False
+        r = praw.Reddit('/u/powderblock Glasses Bot')
+        r.login('DealWithItbot', password)
+        for post in r.get_subreddit('all').get_new(limit=20):
+                if post not in already_done:
+                        already_done.append(post)
+                        if "imgur.com" in post.url and (".jpg" in post.url or ".png" in post.url or ".gif" in post.url):
+                                        print post.url
+                                        response = urllib.urlopen(str(post.url))
+                                        # load the image we want to detect features on
+                                        # Convert rawImage to Mat
+                                        filearray = np.asarray(bytearray(response.read()), dtype=np.uint8)
+                                        frame = cv2.imdecode(filearray, cv2.CV_LOAD_IMAGE_UNCHANGED)
+                                        try:
+                                                if frame.shape[0] > 5000 or frame.shape[1] > 5000: print("Image is too large, skipping.")
+                                                else: a = 1 / 0
+                                        except:
+                                                minisize = (frame.shape[1]/DOWNSCALE,frame.shape[0]/DOWNSCALE)
+                                                miniframe = cv2.resize(frame, minisize)
+                                                eyes = eyeClass.detectMultiScale(miniframe)
+                                                if len(eyes) > 0:
+                                                        print str(post.url)
+                                                        foundImage = True
+                                                        break
 
-        while True:
-            # key handling (to close window)
-            key = cv2.waitKey(20)
-            if key in [27, ord('Q'), ord('q')]: # exit on ESC
-                cv2.destroyWindow("Loading Image Buffer From a URL")
-                break
+        # Only process if the image was found
+        if foundImage:
+                for eye in eyes:
+                    x, y, w, h = [v * DOWNSCALE for v in eye]
+                    h = w / ratio
+                    y += h / 2
+                    # resize glasses to a new var called small glasses
+                    smallglasses = cv2.resize(glasses, (w, h))
+                    # the area you want to change
+                    bg = frame[y:y+h, x:x+w]
+                    bg *= np.atleast_3d(255 - smallglasses[:, :, 3])/255.0
+                    bg += smallglasses[:, :, 0:3] * np.atleast_3d(smallglasses[:, :, 3])
+                    # put the changed image back into the scene
+                    frame[y:y+h, x:x+w] = bg
+                print("Found image. Writing image.")
+                cv2.imwrite(str(post.url), frame)
 
-if not foundImage:
-        print("No Image found.")
+        if not foundImage:
+                print("No image with eyes found.")
+        time.sleep(30)
