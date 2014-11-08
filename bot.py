648b6b45b653e3608a2b2a71ad1b7907156da2c9
Blade Nelson <powderblock@gmail.com>
2014-11-08 15:50:02 -0500

    Remove broken error check

--- bot.py
+++ bot.py
@@ -125,9 +125,7 @@ while True:
             if is_imgur_url(post.url):
                 filename = str(post.url).replace(":", "").replace("/", "")
                 foundImage = True
-                try: response = urllib.urlopen(post.url)
-                except URLError as e:
-                        print e.reason
+                response = urllib.urlopen(post.url)
 
                 # load the image we want to detect features on
                 # Convert rawImage to Mat
